f5e13f8e6e99b03827092dc77ab3c0e4d4eabcd2
lbinria <B>
2025-07-13 11:35:02 +0200

    feat: transform binding and assignment to chip ast

--- lib/ast.ml
+++ lib/ast.ml
@@ -35,28 +35,52 @@ type chip_expr_list =
   | ChipExprList of chip_expr * chip_expr_list
 and chip_expr = 
   | CLS (* 00E0 *)
-  | LD_Vx_Byte of int * int (* 3xkk *)
-  | DRW of int * int * int (* Dxyn *)
-  | LD_I_addr (* Annn *)
+  | LD_Vx_Byte of register_num * int (* 3xkk *)
+  | DRW of register_num * register_num * int (* Dxyn *)
+  | LD_I_addr of int (* Annn *)
   | END
 
 let rec transform data = function 
   | ExprList (e, l) -> ChipExprList (transform_expr data e, transform data l)
   | Expr e -> ChipExpr (transform_expr data e)
+
 and transform_expr data = function 
   | Clear -> CLS
-  | Binding (var, a) -> Hashtbl.replace data.bindings var (transform_assignment data a); END
-  | _ -> END
-and transform_assignment data = function 
-    | RegAssignment (reg, v) -> Hashtbl.replace data.variables (Val reg) v; Val reg
-    | VarAssignment (var, v) -> Hashtbl.replace data.variables (Var var) v; Var var
+  | Binding (var, a) -> 
 
+      (match (transform_assignment data a) with
+      | RegAssignment (reg, v) -> 
+          Hashtbl.replace data.bindings var (Val reg);
+          LD_Vx_Byte (reg, v)
+
+      | VarAssignment (var, v) ->
+          Hashtbl.replace data.bindings var (Var var);
+          (* Search reg from variable *)
+          let reg = 0 in 
+          LD_Vx_Byte (reg, v)
+      )
+  
+  | _ -> END
+
+and transform_assignment data = function 
+    | RegAssignment (reg, v) as x -> 
+        Hashtbl.replace data.variables (Val reg) v; 
+        x
+        (* (LD_Vx_Byte (reg, v), Val reg) *)
+
+    | VarAssignment (var, v) as x -> 
+      Hashtbl.replace data.variables (Var var) v; 
+      x
+      
+
+(* Compile chip AST (transform to hex string) *)
 let rec compile = function 
   | ChipExpr e -> compile_expr e
   | ChipExprList (e, l) -> compile_expr e ^ compile l
+
 and compile_expr = function 
   | CLS -> "00E0"
   | LD_Vx_Byte (x, kk) -> "6" ^ Printf.sprintf "%x" x ^ Printf.sprintf "%02x" kk
   | DRW (x, y, n) -> "D" ^ Printf.sprintf "%x" x ^ Printf.sprintf "%x" y ^ Printf.sprintf "%x" n
-  | LD_I_addr -> ""
+  | LD_I_addr nnn -> "A" ^ Printf.sprintf "%03x" nnn
   | END -> ""
